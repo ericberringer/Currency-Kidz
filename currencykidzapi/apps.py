5cc95ddd4a18860c1d324019da6774596050e28b
ericberringer <eric.berringer@gmail.com>
2021-06-08 11:44:06 -0500

    first commit

---
+++ currencykidzapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CurrencykidzapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'currencykidzapi'
---
+++ currencykidzapi/models/__init__.py
@@ -0,0 +1,7 @@
+import .saver from
+import .withdrawal_event from
+import .deposit_event from
+import .currency from
+import .quiz from
+import .quiz_images from
+import .question from
