fd93452bb3f92888be0e0218b726f75c5054465c
ericberringer <eric.berringer@gmail.com>
2021-07-09 09:49:44 -0500

    registered models django admin.

--- currencykidzapi/admin.py
+++ currencykidzapi/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Saver, Currency, DepositEvent, WithdrawalEvent, Question, Quiz, QuizImages
 
 # Register your models here.
+admin.site.register(Saver)
+admin.site.register(Currency)
+admin.site.register(DepositEvent)
+admin.site.register(WithdrawalEvent)
+admin.site.register(Question)
+admin.site.register(Quiz)
+admin.site.register(QuizImages)
